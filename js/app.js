389419687069544d67f38fac4d0b43100472f786
Robert.M.Kemper <54542188+Robert-M-Kemper@users.noreply.github.com>
2019-09-26 10:41:32 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+    var x = document.getElementById("firstName").value;
+    var y = document.getElementById("lastName").value;
+    document.getElementById("demo").innerHTML ="Hello " + x + " " + y;
+}
